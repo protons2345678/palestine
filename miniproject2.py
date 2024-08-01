31a5451d9d64832d9e1479b5347c8240ff795353
Rowaida ali <rowaidaali396@gmail.com>
2024-08-01 23:20:56 +0300

    changes

--- miniproject2.py
+++ miniproject2.py
@@ -1,32 +1,65 @@
+import turtle as t
+w=t.Screen()
+w.title(" Our pong game")
+w.bgcolor("pink")
+w.setup(width=780,height=580)
+w.tracer(0)  #turn  automatic screen updates on or off
+
+#create first player "left paddele"
+leftpaddle=t.Turtle()
+leftpaddle.speed(0)
+leftpaddle.shape("square")
+leftpaddle.color("white")
+leftpaddle.shapesize(stretch_len=1,stretch_wid=5)
+leftpaddle.penup()
+leftpaddle.goto(x=350,y=0)
+
+#create second player "right paddele"
+rightpaddle=t.Turtle()
+rightpaddle.speed(0)
+rightpaddle.shape("square")
+rightpaddle.color("white")
+rightpaddle.shapesize(stretch_len=1,stretch_wid=5)
+rightpaddle.penup()
+rightpaddle.goto(x=-350,y=0)
+
+
+#creating a ball
+ball=t.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("black")
+ball.penup() 
+ball.goto(x=5,y=5)
 #score
 score1 = 0
 score2 = 0
-score = turtle.Turtle
+score=t.Turtle()
 score.speed(0)
-score.color("white")
 score.penup()
-score.hideturtle
-score.goto(0,0)
-score.write("Playrr 1: 0 Player 2: 0", align="center", font=("Courier",24,"normal"))
+score.hideturtle()
+score.goto(x=0,y=260)
+score.write( "player1=0,player2=0",align="center" ,)
+score.hideturtle()
 
 #functions
 def paddle1_up():
-    y = paddle1.ycor() #set the y coordinate of the paddle1
-    y += 20 #set the y coordinate of the paddle2
-    paddle1.sety(y) #set the y of the paddle1 to the new y coordinate
+    y = rightpaddle.ycor() #set the y coordinate of the paddle1
+    y += 90 #set the y coordinate of the paddle2
+    rightpaddle.sety(y) #set the y of the paddle1 to the new y coordinate
 def paddle1_down():
-    y = paddle1.ycor() #set the y coordinate of the paddle1
-    y -= 20 #set the y to decrease be20
-    paddle1.sety(y) #set the y of the paddle1 to the new y coordinate
+    y = rightpaddle.ycor() #set the y coordinate of the paddle1
+    y -= 90 #set the y to decrease be20
+    rightpaddle.sety(y) #set the y of the paddle1 to the new y coordinate
 
 def paddle2_up():
-    y = paddle2.ycor() #set the y coordinate of the paddle2
-    y += 20 #set the y coordinate of the paddle2
-    paddle2.sety(y) #set the y of the paddle2 to the new y coordinate
+    y = leftpaddle.ycor() #set the y coordinate of the paddle2
+    y += 90 #set the y coordinate of the paddle2
+    leftpaddle.sety(y) #set the y of the paddle2 to the new y coordinate
 def paddle2_down():
-    y = paddle2.ycor()  #set the y coordinate of the paddle2
-    y -= 20 #set the y coordinate of the paddle2
-    paddle2.sety(y) #set the y of the paddle2 to the new y coordinate
+    y = leftpaddle.ycor()  #set the y coordinate of the paddle2
+    y -= 90 #set the y coordinate of the paddle2
+    leftpaddle.sety(y) #set the y of the paddle2 to the new y coordinate
 
 #keyboard blindings
 w.listen() #tell the window to expect keyboard input
@@ -36,8 +69,8 @@ w.onkeypress(paddle2_up, "Up") #when pressing Up the function is invoked
 w.onkeypress(paddle2_down, "Down") #when pressing Up the function is invoked
 
 #ball
-ball.dx = 2.5
-ball.dy = 2.5
+ball.dx = 0.5
+ball.dy = 0.5
 
 #main game loop
 while True:
@@ -61,7 +94,7 @@ while True:
         ball.dx *= -1 #reverse the x direction
         score1 += 1
         score.clear()
-        score.write("Playrr 1: {} Player 2: {}".format(score1,score2), align="center", font=("Courier",24,"normal"))
+        score.write("Player 1: {} Player 2: {}".format(score1,score2), align="center", font=("Courier",24,"normal"))
 
     if ball.xcor() <- 390: #if ball is at left border
         ball.goto(0,0) #return ball to center
@@ -70,10 +103,11 @@ while True:
         score.clear()
         score.write("Playrr 1: {} Player 2: {}".format(score1,score2), align="center", font=("Courier",24,"normal"))
     #tasadom paddle and ball
-    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.xcor() < paddle2.ycor() + 40 and ball.ycor() > paddle2.ycor() - 40):
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.xcor() < leftpaddle.ycor() + 40 and ball.ycor() > leftpaddle.ycor() - 40):
         ball.setx(340)
         ball.dx *= -1
 
-    if (ball.xcor() > -340 and ball.xcor() < -350) and (ball.xcor() < paddle1.ycor() + 40 and ball.ycor() > paddle1.ycor() - 40):
+    if (ball.xcor() > -340 and ball.xcor() < -350) and (ball.xcor() < rightpaddle.ycor() + 40 and ball.ycor() > rightpaddle.ycor() - 40):
         ball.setx(-340)
         ball.dx *= -1
+        
